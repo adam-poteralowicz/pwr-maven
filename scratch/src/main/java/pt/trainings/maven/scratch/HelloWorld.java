323e9844d031a02474e1e7adc4682cd1e1c0a1b5
Bres1990 <adam.poteralowicz@gmail.com>
2014-11-26 06:03:45 +0100

    Maven exercises.

---
+++ scratch/src/main/java/pt/trainings/maven/scratch/HelloWorld.java
@@ -0,0 +1,10 @@
+package pt.trainings.maven.scratch;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Greetings greetings = new Greetings();
+		System.out.println(greetings.sayHello(args[0]));
+	}
+
+}
